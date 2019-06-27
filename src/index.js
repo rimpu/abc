3e8845411dfdd5f844f6a8c9871e140f5d3750e5
rimpu <aditibose1995@gmail.com>
2019-06-27 10:05:36 +0530

    Added socketio

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const http = require('http')
+const socketio = require('socket-io')
 const app = express()
 const path = require('path')
+const server = http.createServer(app)
+const io = socketio(server)
 const publicDirectory = path.join(__dirname,"../public")
 console.log(publicDirectory)
 app.use(express.static(publicDirectory))
 
-app.listen(process.env.PORT,()=>{
+server.listen(process.env.PORT,()=>{
     console.log('Server started listening at port no '+ process.env.PORT)
 })
