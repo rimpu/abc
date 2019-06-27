5d9141ec59756802ebe2559837cb6a73192e890b
rimpu <aditibose1995@gmail.com>
2019-06-27 10:16:03 +0530

    First connection from client to server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const http = require('http')
-const socketio = require('socket-io')
+const socketio = require('socket.io')
 const app = express()
 const path = require('path')
 const server = http.createServer(app)
@@ -8,6 +8,10 @@ const io = socketio(server)
 const publicDirectory = path.join(__dirname,"../public")
 console.log(publicDirectory)
 app.use(express.static(publicDirectory))
+
+io.on('connection',()=>{
+    console.log("New web socket connection")
+})
 
 server.listen(process.env.PORT,()=>{
     console.log('Server started listening at port no '+ process.env.PORT)
